e2f76670c99ac80b90812a3d6cf3ea57a6763e5c
mhmdaz <muhammadazeez@zoho.com>
2025-06-19 03:00:14 +0530

    add swagger api annotations in TokenController

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -7,6 +7,35 @@ use Illuminate\Support\Facades\Auth;
 
 class TokenController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/tokens/create",
+     *     tags={"Tokens"},
+     *     summary="Assign new tokens to users",
+     *     description="Assign new tokens to users",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"email","password"},
+     *             @OA\Property(property="email", type="string", example="john@example.com"),
+     *             @OA\Property(property="password", type="string", example="password"),
+     *             @OA\Property(property="token_name", type="string", example="some_random_token_name")
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Token assigned successfully"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity"
+     *     )
+     * )
+     */
     public function create(Request $request)
     {
         $credentials = $request->validate([
